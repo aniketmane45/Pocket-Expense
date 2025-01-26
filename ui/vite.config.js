348cf9a6b397be7f2ec58149cd9508a3a29681cd
aniketmane45 <aniketmane692@gmail.com>
2025-01-26 11:01:31 +0530

    vite.config.js

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // This allows external access
-    port: 5174,        // Keep your port or change to another one if needed
+    host: '0.0.0.0',  // Exposes to the network
+    port: 5176,        // Set a fixed port
   }
 })
